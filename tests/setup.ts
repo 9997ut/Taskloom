b563800f7adde6d8f3c98bad0960f01d9b134f54
9997ut <ujjwaltomar9997@gmail.com>
2026-08-09 10:33:18 +0530

    test: add mongodb-memory-server test setup with helpers

---
+++ tests/setup.ts
@@ -0,0 +1,26 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import app from '../server/app';
+
+let mongoServer: MongoMemoryServer;
+
+export async function setupTestDB() {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  await mongoose.connect(uri);
+}
+
+export async function teardownTestDB() {
+  await mongoose.connection.dropDatabase();
+  await mongoose.disconnect();
+  await mongoServer.stop();
+}
+
+export async function clearCollections() {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    await collections[key].deleteMany({});
+  }
+}
+
+export { app };
