882ffb3f6ea0fb3b0fce87f8d6bb6f7ded1015f6
9997ut <ujjwaltomar9997@gmail.com>
2026-08-10 23:08:33 +0530

    refactor(tests): move backend tests to tests/server/ per spec structure

--- tests/server/setup.ts
+++ tests/server/setup.ts
@@ -1,6 +1,6 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import app from '../server/app';
+import app from '../../server/app';
 
 let mongoServer: MongoMemoryServer;
 
